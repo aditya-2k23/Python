56762c27903641c5af5b0276e7ead8d6010079ec
Aditya <holaaditya123@gmail.com>
2023-11-24 11:29:22 +0530

    Deleted unnecessary files and code

---
+++ Python Projects/python to whatsapp/main.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.append("C:/Users/Aditya/AppData/Local/Programs/Python/Python311/python.exe")
+
+import pywhatkit as pyw
+
+pyw.sendwhatmsg("+916284219106", "Welcome to Python", 22, 7)
