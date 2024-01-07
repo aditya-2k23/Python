bc4103a12209c93d0662a00be38afff4399ae9d4
Aditya <holaaditya123@gmail.com>
2024-01-07 17:15:57 +0530

    new files

---
+++ Python Projects/Automation/multiClipboard/multiclipboard.py
@@ -0,0 +1,18 @@
+import sys
+import clipboard
+import json
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    print(command)
+
+    if command == 'save':
+        print("save")
+    elif command == 'load':
+        print("load")
+    elif command == 'list':
+        print("list")
+    else:
+        print("Unknown command!")
+else:
+    print("Please pass exactly one command!")
