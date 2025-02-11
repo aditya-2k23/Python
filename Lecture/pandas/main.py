737695644ac9a58acc27a6f7b3498df4cbc45c92
aditya-2k23 <holaaditya123@gmail.com>
2025-02-11 19:09:33 +0530

    Add examples for creating, appending, and deleting rows in DataFrame

--- Lecture/pandas/main.py
+++ Lecture/pandas/main.py
@@ -41,3 +41,23 @@ print(d)
 
 d1 = pd.DataFrame(b, columns=["Roll No"])
 print(d1)
+
+
+### ! Dataframe from list
+d = {"one": pd.Series([1, 2, 3], index=['a', 'b', 'c']), "two": pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
+df = pd.DataFrame(d)
+print(df[2:4])
+
+### ! Addition of rows
+df = pd.DataFrame([[1, 2], [3, 4]], columns=['a', 'b'])
+df2 = pd.DataFrame([[5, 6], [7, 8]], columns=['a', 'b'])
+df = df._append(df2)
+print(df)
+
+### ! Deletion of rows
+df = pd.DataFrame([[1, 2], [3, 4]], columns=['a', 'b'])
+df2 = pd.DataFrame([[5, 6], [7, 8]], columns=['a', 'b'])
+df = df._append(df2)
+
+df = df.drop(0)
+print(df)
