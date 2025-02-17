79e0e28db22dc9dc25d3af3e42f4bda1ee252570
aditya-2k23 <holaaditya123@gmail.com>
2025-02-17 21:11:42 +0530

    Fix array initialization in pandas Series example

--- Lecture/pandas/series.py
+++ Lecture/pandas/series.py
@@ -5,13 +5,11 @@ l = [12, 25, 56, 88]
 df = pd.DataFrame(l, columns=['RollNo'])
 print(df)
 
-data = np.array([90, 75, 50, 66])
+data = np.array([90, 75], [50, 66])
 s = pd.Series(data)
 print(s)
 
 data = np.array(['a', 'b', 'c', 'd'])
-
-
 
 data = {'a': 0., 'b': 1., 'c': 2.}
 s = pd.Series(data)
