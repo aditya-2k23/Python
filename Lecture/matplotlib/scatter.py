76cb42d5c3b9fe2cdab90328d1f11c3c008ab884
aditya-2k23 <holaaditya123@gmail.com>
2025-03-04 11:19:22 +0530

    Add scatter plot example to matplotlib with labeled axes and legend

---
+++ Lecture/matplotlib/scatter.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 15, 25, 30]
+
+plt.plot(x, y, marker='o', linestyle='-', color='g', label='Lines')
+plt.scatter(x, y, color='r', marker='o', s=100, label='Points')
+
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Scatter Plot Diagram')
+
+plt.legend() # Legend is used to identify the plot lines
+plt.show()
