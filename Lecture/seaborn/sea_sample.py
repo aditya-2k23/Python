afe5d078b009aefa03b17eeb4256c0719ee4cb50
aditya-2k23 <holaaditya123@gmail.com>
2025-03-20 11:31:36 +0530

    Enhance data visualizations with additional plots for sales and profit analysis using Seaborn

--- Lecture/seaborn/sea_sample.py
+++ Lecture/seaborn/sea_sample.py
@@ -2,7 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-file_path = "./sample_superstore.xls"
+file_path = "D:/Study/Codes2/Python/Lecture/seaborn/sample_superstore.xls"
 data = pd.read_excel(file_path, sheet_name='samplesuperstore')
 
 #Preview the data
@@ -15,17 +15,30 @@ plt.figure(figsize=(10,5))
 data['Order Date'] = pd.to_datetime(data['Order Date'])
 sns.lineplot(data=data, x='Order Date',y='Sales')
 plt.title('Sales Trend Over Time')
-# plt.show()
+plt.show()
 
 #2. Bar Plot - Average Profit by Category
 plt.figure(figsize=(8,5))
-barplot = sns.barplot(x=data["Category"],y=data["Profit"],errorbar=None)
+barplot = sns.barplot(data=data, x="Category",y="Profit",errorbar=None)
 for p in barplot.patches:
     value = f'{p.get_height():.2f}' #2 decimal Places
     barplot.annotate(value,
                      (p.get_x()+ p.get_width()/2., p.get_height()),
-                     he='center', va='center', fontsize=10, color='black',
+                     ha='center', va='center', fontsize=10, color='black',
                      xytext=(0,5), textcoords='offset points')
 
 plt.title('Average profit by category')
+plt.show()
+
+#3. Scatter plot - Savles vs Profit, colored by Ship Mode
+plt.figure(figsize=(8, 5))
+sns.scatterplot(data=data, x="Sales", y="Profit", hue="Ship Mode", style="Ship Mode", s=100)
+plt.title("Sales vs Profit by Ship Mode")
+plt.show()
+
+#4. Heatmap - Correlation Between Numerical Fields
+plt.figure(figsize=(6, 4))
+corr = data[["Sales", "Profit", "Quantity", "Discount"]].corr()
+sns.heatmap(corr, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+plt.title("Correlation Heatmap")
 plt.show()
