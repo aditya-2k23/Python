d0e15b3449255a2ccfdf4a5dcf58d5bb9cc85808
aditya-2k23 <holaaditya123@gmail.com>
2025-03-25 11:48:09 +0530

    Enhance histogram transparency for better visualization clarity

--- Lecture/matplotlib/histogram.py
+++ Lecture/matplotlib/histogram.py
@@ -3,8 +3,7 @@ import numpy as np
 
 data = np.random.randn(100)
 
-plt.hist(data, bins=10, color='hotpink', edgecolor='white')
-
+plt.hist(data, bins=10, color='hotpink', edgecolor='white', alpha=0.7)
 plt.title('Basic Histogram')
 plt.xlabel('Value')
 plt.ylabel('Frequency')
