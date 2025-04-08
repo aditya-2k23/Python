2a87c59362583a70b211efd3d381a74a2fd60bed
aditya-2k23 <holaaditya123@gmail.com>
2025-04-08 11:25:53 +0530

    Refactor z-score calculation to use pandas DataFrame for standard deviation and improve output formatting

--- Lecture/practice/z-score.py
+++ Lecture/practice/z-score.py
@@ -1,10 +1,13 @@
 import numpy as np
+import pandas as pd
 
 data = np.array([10, 12, 14, 15, 18, 21, 22, 25, 28, 30, 2, -10])
 
 mean = np.mean(data)
-std_dev = np.std(data)
+df = pd.DataFrame(data, columns=['Data'])
+# print(df.describe())
+std_dev = df['Data'].describe()['std']
 print(f"Mean: {mean:.2f}, Standard Deviation: {std_dev:.2f}")
 z_scores = (data - mean) / std_dev
-for i, z in enumerate(z_scores):
-    print(f"Z_{i + 1} = {z:.2f}")
+for i, j in zip(data, z_scores):
+    print(f"Z_{i} = {j:.2f}")
