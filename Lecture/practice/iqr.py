d724c54cc7cfe50f5b50ba3ff1c4af0df5a575bc
aditya-2k23 <holaaditya123@gmail.com>
2025-04-08 10:39:44 +0530

    Refactor IQR calculation to use numpy array and simplify outlier detection

--- Lecture/practice/iqr.py
+++ Lecture/practice/iqr.py
@@ -1,13 +1,15 @@
 import numpy as np
 
-data = [10, 12, 14, 15, 18, 21, 22, 25, 28, 30, 2, -10]
-sort = sorted(data)
+data = np.array([10, 12, 14, 15, 18, 21, 22, 25, 28, 2, -10])
 
-q1 = np.percentile(sort, 25)
-q3 = np.percentile(sort, 75)
+q1 = np.percentile(data, 25)
+q3 = np.percentile(data, 75)
 iqr = q3 - q1
 
 print("Q1:", q1, "Q3:", q3, "IQR:", iqr)
 lower = q1 - 1.5 * iqr
 upper = q3 + 1.5 * iqr
 print("Lower Bound:", lower, "Upper Bound:", upper)
+
+outliers_iqr = data[(data < lower) | (data > upper)]
+print(outliers_iqr)
